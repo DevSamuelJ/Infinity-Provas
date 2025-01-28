a57867022297135edace15b5ce196f5a70ee3f37
samuelljps <samuel701silva@gmail.com>
2025-01-28 14:20:53 -0300

    Prova de revisão em lógica feita com sucesso!

---
+++ Provas/Prova Revisão da logica da programação/prova_main.py
@@ -0,0 +1,12 @@
+produtos = {}
+contador = 0
+while contador < 5:
+  nome_produto= input("digite o nome do produto: ")
+  preco_produto= float(input("digite o preco do produto: "))
+  produtos[nome_produto] = preco_produto
+  print(f"depois de inserir:, {produtos}")
+  contador+=1
+total = 0
+for chave_do_dicionario in produtos:
+  total += produtos[chave_do_dicionario]
+print(f"total com a soma:, {total}")
